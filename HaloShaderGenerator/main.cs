c853c6ca4de8b62c808be1897e1ef01b02081712
Haydn Trigg <me@haydntrigg.com>
2018-02-28 17:21:12 +1030

    HLSL Files and Enums

--- HaloShaderGenerator/main.cs
+++ HaloShaderGenerator/main.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TagTool.Utilities;
 
 namespace HaloShaderGenerator
 {
@@ -10,13 +12,16 @@ namespace HaloShaderGenerator
     {
         static void Main()
         {
+            var src = File.ReadAllText("pixel.hlsl");
+            var defines = new DirectXUtilities.MacroDefine[] { };
+            if(DirectXUtilities.CompilePCShader(src, defines, "", "main", "ps_3_0", 0, out byte[] shader, out string errors, out string constanttable))
+            {
 
-
-
-
-
-
-
+            }
+            else
+            {
+                throw new Exception(errors);
+            }
 
         }
     }
